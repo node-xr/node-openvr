f4bcffc3820986b084217d8d7a4c485126d3388e
Pras Velagapudi <psigen@gmail.com>
2017-05-17 03:16:18 -0400

    Added unit tests!

--- test/test_index.js
+++ test/test_index.js
@@ -1,12 +1,30 @@
 const assert = require('assert');
 const openvr = require('../index');
 
-describe('openvr', function() {
-  describe('#VR_Init()', function() {
-    it('should return an IVRSystem.', function() {
+describe('openvr', () => {
+  describe('#VR_Init()', () => {
+    it('should return an IVRSystem.', () => {
       const system = openvr.VR_Init(openvr.EVRApplicationType.VRApplication_Utility);
       assert.ok(system);
       openvr.VR_Shutdown();
+    });
+  });
+});
+
+describe('IVRSystem', () => {
+  var ivrsystem;
+  before(() => {
+    ivrsystem = openvr.VR_Init(openvr.EVRApplicationType.VRApplication_Utility);
+  });
+  after(() => {
+    openvr.VR_Shutdown();
+  });
+
+  describe('#GetRecommendedRenderTargetSize', () => {
+    it('should return a width and height.', () => {
+      const result = ivrsystem.GetRecommendedRenderTargetSize();
+      assert.ok(result.width);
+      assert.ok(result.height);
     });
   });
 });
