0b5294265f37776dd4bf1b8c26e831d1b88480e1
CreeJee <000as00060@gmail.com>
2021-05-23 18:35:18 +0900

    add typing for d.ts

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -7,36 +7,7 @@ void Initialize(
   v8::Local<v8::Object> exports,
   v8::Local<v8::Value> module,
   v8::Local<v8::Context> context
-) {
-  
-  // Isolate* isolate = context->GetIsolate();
-  // exports->Set(context,
-  //   Nan::New("VR_Init").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_Init)->GetFunction(context));
-  // exports->Set(&context,
-  //   Nan::New("VR_Shutdown").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_Shutdown)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_IsHmdPresent").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_IsHmdPresent)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_IsRuntimeInstalled").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_IsRuntimeInstalled)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_RuntimePath").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_RuntimePath)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_GetVRInitErrorAsSymbol").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_GetVRInitErrorAsSymbol)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_GetVRInitErrorAsEnglishDescription").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_GetVRInitErrorAsEnglishDescription)->GetFunction(context));
-  // exports->Set(context,
-  //   Nan::New("VR_GetInitToken").ToLocalChecked(),
-  //   Nan::New<v8::FunctionTemplate>(VR_GetInitToken)->GetFunction(context)
-  // );
-    
-    
+) { 
   Nan::Set(
     exports,
     Nan::New("VR_Init").ToLocalChecked(),
@@ -65,7 +36,6 @@ void Initialize(
   Nan::Set(
     exports,
     Nan::New("VR_GetVRInitErrorAsSymbol").ToLocalChecked(),
-    // Nan::New<v8::FunctionTemplate>(VR_GetVRInitErrorAsSymbol)->GetFunction(context)
     Nan::GetFunction(Nan::New<v8::FunctionTemplate>(VR_GetVRInitErrorAsSymbol)).ToLocalChecked()
   );
   Nan::Set(
